1bd4efb4d0855886e261835c50eaa2b2db90e658
BrendenDurand <51679354+BrendenDurand@users.noreply.github.com>
2022-07-14 10:44:58 +0200

    Update SendEmail.java

--- src/services/SendEmail.java
+++ src/services/SendEmail.java
@@ -7,17 +7,25 @@ import javax.mail.internet.*;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+//not bad naming but if this hosts all the sending of all emails its better to naming it something like EmailSendingManager if its user specific name it as such
 public class SendEmail {
 
     String senderEmail;
-    String senderPassword;
+    String senderPassword;//can be in an object
     final String emailSMTPserver = "smtp.gmail.com";
-    final String emailServerPort = "465";
-    String receiverEmail = null;
-    static String emailSubject;
-    static String emailBody;
+    final String emailServerPort = "465";//can be in an object or a config
+    String receiverEmail = null;//can be in an object or config
+    static String emailSubject;//can be in an object
+    static String emailBody;//can be in an object
 
+    
+    /*
+    So this method is not bad at all its a good method depending on the use case but the naming could be checked if its specific for users then name 
+    it as such , if its a generic method for sending all emails its a good idea to name it in a way or write a comment to share that
+    
+    In the method nothing is wrong with it but it can be abstracted a bit for scalabilty and ease of use. Java 101 says 1 mehod does 1 thing, 1 class does 1 thing 
+    try to always use this principle for the future. 
+    */
     public SendEmail(String email, String subject, String body) {
         receiverEmail = email;
         emailSubject = subject;
