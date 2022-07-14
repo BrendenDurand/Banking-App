5c3f942f53703305ed7bd90d2391f79b63f56762
BrendenDurand <51679354+BrendenDurand@users.noreply.github.com>
2022-07-14 10:46:30 +0200

    Update generateBalance.java

--- src/services/generateBalance.java
+++ src/services/generateBalance.java
@@ -1,7 +1,7 @@
 package services;
 
 import java.util.Random;
-
+//why is the class called generate balance ? a class is not a method I would look at an class called AccountManager and in that class you have generate balance, save balance etc
 public class generateBalance {
 
     public Double createBalance() {
