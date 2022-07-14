d7417a341375a965b92ab9bf71c202af4553ce0f
BrendenDurand <51679354+BrendenDurand@users.noreply.github.com>
2022-07-14 10:34:13 +0200

    Update Account.java

--- src/models/Account.java
+++ src/models/Account.java
@@ -1,11 +1,12 @@
 package models;
-
+//Take a look at adding the lombok depedancy to your pom file , its a really amazing library that saves you alot of time (eg no need to generate getters and setters)
+//What type of account is this ? Is it the generic object which wil have shared fields through inheritance or is it a child account , we can discuss this in depth through a design doc
 public class Account {
 
     private int id;
-    private Double balance;
+    private Double balance;//what balance ? currentbalance, availableBalance, owedBalance? 
     private int userID;
-    public User user;
+    public User user;//Why is this here? Do accounts have users ? Users should have a list of Accounts 
 
     public int getId() {
         return id;
